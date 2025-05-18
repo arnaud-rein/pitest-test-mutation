244576767183cfe26d678657091eed7144341eee
arnaud-rein <arnaud.rein029@gmail.com>
2025-05-18 20:38:36 +0200

    ETAPE 1 : Code de base

--- demo/src/main/java/com/example/demo/ReductionMagasin.java
+++ demo/src/main/java/com/example/demo/ReductionMagasin.java
@@ -4,17 +4,17 @@ public class ReductionMagasin {
 
 
     public boolean estElligibleReduction(double montantArticle, boolean carteMembre) {
-        if (montantArticle <= 100.0) {
+        if (montantArticle > 100.0) {
             return true;
         }
-        else if(montantArticle <= 80 ) {
+        else if(montantArticle > 80 ) {
             if(carteMembre == true) {
                 return true;
             }else{
                 return false;
             }
 
-        } else if(montantArticle >= 80  && carteMembre == true) {
+        } else if(montantArticle < 80  && carteMembre == true) {
             return true;
         }
 
